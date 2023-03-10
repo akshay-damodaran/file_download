ef088844347af7f59e882d938615564791c0f79e
Akshay Damodaran <akshay.damodaran2411@gmail.com>
2023-03-11 01:27:38 +0530

    Backend code

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
+const base_path = "/Users/akshay/Projects/file_download/"
+const file1_path = "/Users/akshay/Projects/file_download/sample_img.png"
+const file2_path = "/Users/akshay/Projects/file_download/sample_pdf.pdf"
+
+app.use(cors())
+
+app.get('/', (_, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
+
+app.get('/list_files', (_, res) => {
+  res.json([
+    {
+      file_name: "sample_img.png",
+      file_path: file1_path
+    },
+    {
+      file_name: "sample_pdf.pdf",
+      file_path: file2_path
+    }
+  ])
+})
+
+app.get('/get_file/:file_name', (req, res) => {
+  console.log(req.params.file_name)
+  file_name = req.params.file_name
+  res.download(base_path + file_name, file_name)
+})
